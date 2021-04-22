64d6599bdd9fae7bfbf1ba75d9aebf5e4b805322
Nikolay <nikolay.topalov@gmail.com>
2021-04-22 18:20:44 +0300

    typo

--- config/config.js
+++ config/config.js
@@ -3,11 +3,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 9000,
-        db: 'mongodb+srv://xfce:niki654123@cluster0.ltcnh.azure.mongodb.net/phone-shop?retryWrites=true&w=majority',
-        authCookieName: 'xfcEEcfx',
-        authHeaderName: 'xfcEEcfx',
+        db: '',
+        authCookieName: '',
+        authHeaderName: '',
         saltRounds: 9,
-        jwtSecret: 'Phone_JWT_Secret'
+        jwtSecret: ''
     },
     production: {}
 };
